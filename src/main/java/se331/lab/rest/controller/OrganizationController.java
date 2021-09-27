ab40f00aa57d8efb8d203954ecd5c1d9fe2c6c1c
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-09-27 16:10:10 +0700

    3.2 add list of imageUrls and getmapping by id

--- src/main/java/se331/lab/rest/controller/OrganizationController.java
+++ src/main/java/se331/lab/rest/controller/OrganizationController.java
@@ -23,16 +23,16 @@ public class OrganizationController {
         return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
     }
 
-//    @GetMapping("organizers/{id}")
-//    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
-//
-//        Organizer output = organizerService.getOrganizer(id);
-//        if (output != null) {
-//            return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(output));
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
-//        }
-//    }
+    @GetMapping("organizers/{id}")
+    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
+
+        Organizer output = organizerService.getOrganizer(id);
+        if (output != null) {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(output));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
+    }
 
     @PostMapping("/organizers")
     public ResponseEntity<?> addEvent(@RequestBody Organizer organizer) {
